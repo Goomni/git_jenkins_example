864b4258fc62533756139981bacad4701a8c2aa4
Goomni <rnals622@gmail.com>
2021-08-29 16:13:11 +0900

    파워쉘 Start-Process에 인자 추가

--- AfterPushScript.ps1
+++ AfterPushScript.ps1
@@ -1,2 +1,2 @@
 ﻿dotnet build -c Release .\git_jenkins_example.sln
-Start-Process .\git_jenkins_example\bin\Release\net5.0\git_jenkins_example.exe
+Start-Process -FilePath .\git_jenkins_example\bin\Release\net5.0\git_jenkins_example.exe
