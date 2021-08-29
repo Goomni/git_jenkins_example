dfd69c6a34a8945655afbee0cf93d88a9d1a1530
Goomni <rnals622@gmail.com>
2021-08-29 16:24:50 +0900

    PsExec 사용해서 결과 프로세스 실행하도록 수정

--- AfterPushScript.ps1
+++ AfterPushScript.ps1
@@ -1,2 +1,2 @@
 ﻿dotnet build -c Release .\git_jenkins_example.sln
-Start-Process -FilePath .\git_jenkins_example\bin\Release\net5.0\git_jenkins_example.exe
+.\PsExec64.exe .\git_jenkins_example\bin\Release\net5.0\git_jenkins_example.exe
