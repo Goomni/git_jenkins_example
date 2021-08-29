949f19546a87a265bf828a66ad5bfd7882d15709
Goomni <rnals622@gmail.com>
2021-08-29 15:21:22 +0900

    깃허브 푸쉬 후 젠킨스 빌드테스트 #1

--- git_jenkins_example/Program.cs
+++ git_jenkins_example/Program.cs
@@ -7,7 +7,8 @@ namespace git_jenkins_example
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("This is GitHub WebHook Test");
+            Console.WriteLine("This is GitHub WebHook Test #1");
+            Console.ReadLine();
         }
     }
 }
